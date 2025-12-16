7b2570ea09fe28f57374ed35616b3ac90d14aded
aliiai <102426663+aliiai@users.noreply.github.com>
2025-12-16 17:48:47 +0200

    ali

--- resources/views/owner/collections/index.blade.php
+++ resources/views/owner/collections/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="w-full mx-auto sm:px-6 lg:px-8">
         <!-- Header -->
         <div class="flex justify-between items-center mb-6">
             <div>
